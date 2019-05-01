b9beb72b1a32e1c48bc3d451c5abc24777b30155
Suparn B <suparn.badwaik@gmail.com>
2019-05-01 12:25:45 -0700

    Programmatic Routing

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-followers',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubFollowersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  submit() {
+    console.log('Submit button clicked');
+    // this.router.navigate(['/posts']);   // redirect to post
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    });
   }
 
 }
