8cd04c65fc53247d739431a657cd65527a4843ba
Suparn B <suparn.badwaik@gmail.com>
2019-05-01 18:54:52 -0700

    Router Link in html with query parameters

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-github-followers',
+  selector: 'github-followers',
   templateUrl: './github-followers.component.html',
   styleUrls: ['./github-followers.component.css']
 })
 export class GithubFollowersComponent implements OnInit {
 
+  followerList: {};
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.followerList = [
+      { id: 1, name:'Captain America' },
+      { id: 2, name:'Iron Man' },
+      { id: 3, name:'Thor' },
+      { id: 4, name:'Thanos' }
+    ];
+    console.log(this.followerList[0].name);
   }
 
   submit() {
